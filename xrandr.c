aeb6706a84aaa95ef9710efe34b4722087a580a1
Guido Serra <zeph@fsfe.org>
2019-03-13 21:57:40 +0100

    set_output_info

--- xrandr.c
+++ xrandr.c
@@ -895,119 +895,113 @@ crtc_can_use_transform (crtc_t *crtc, XTransform *transform) {
  * Report only rotations that are supported by all crtcs
  */
 static Rotation
-output_rotations (output_t *output)
-{
-    Bool	    found = False;
-    Rotation	    rotation = RR_Rotate_0;
-    XRROutputInfo   *output_info = output->output_info;
-    int		    c;
+output_rotations (output_t *output) {
+  Bool            found = False;
+  Rotation        rotation = RR_Rotate_0;
+  XRROutputInfo  *output_info = output->output_info;
+  int             c;
     
-    for (c = 0; c < output_info->ncrtc; c++)
-    {
-  crtc_t	*crtc = find_crtc_by_xid (output_info->crtcs[c]);
-  if (crtc)
-  {
+  for (c = 0; c < output_info->ncrtc; c++) {
+    crtc_t	*crtc = find_crtc_by_xid (output_info->crtcs[c]);
+    if (crtc) {
+      
       if (!found) {
-    rotation = crtc->crtc_info->rotations;
-    found = True;
-      } else
-    rotation &= crtc->crtc_info->rotations;
-  }
-    }
-    return rotation;
+        rotation = crtc->crtc_info->rotations;
+        found = True;
+      } else rotation &= crtc->crtc_info->rotations;
+
+    } // crtc
+  } // c++
+  return rotation;
 }
 
 static Bool
-output_can_use_rotation (output_t *output, Rotation rotation)
-{
-    XRROutputInfo   *output_info = output->output_info;
-    int		    c;
+output_can_use_rotation (output_t *output, Rotation rotation) {
+  XRROutputInfo   *output_info = output->output_info;
+  int              c;
 
-    /* make sure all of the crtcs can use this rotation.
-     * yes, this is not strictly necessary, but it is 
-     * simpler,and we expect most drivers to either
-     * support rotation everywhere or nowhere
-     */
-    for (c = 0; c < output_info->ncrtc; c++)
-    {
-  crtc_t	*crtc = find_crtc_by_xid (output_info->crtcs[c]);
-  if (crtc && !crtc_can_use_rotation (crtc, rotation))
-      return False;
-    }
-    return True;
+  /* make sure all of the crtcs can use this rotation.
+   * yes, this is not strictly necessary, but it is 
+   * simpler,and we expect most drivers to either
+   * support rotation everywhere or nowhere
+   */
+  for (c = 0; c < output_info->ncrtc; c++) {
+    crtc_t	*crtc = find_crtc_by_xid (output_info->crtcs[c]);
+    if (crtc && !crtc_can_use_rotation (crtc, rotation)) return False;
+  } // c++
+  return True;
 }
 
 static Bool
-output_is_primary(output_t *output)
-{
-    if (has_1_3)
-      return XRRGetOutputPrimary(dpy, root) == output->output.xid;
-    return False;
+output_is_primary(output_t *output) {
+  if (has_1_3) return XRRGetOutputPrimary(dpy, root) == output->output.xid;
+  return False;
 }
 
 /* Returns the index of the last value in an array < 0xffff */
 static int
 find_last_non_clamped(CARD16 array[], int size) {
-    int i;
-    for (i = size - 1; i > 0; i--) {
-        if (array[i] < 0xffff)
-      return i;
-    }
-    return 0;
+  int i;
+  
+  for (i = size - 1; i > 0; i--)
+    if (array[i] < 0xffff) return i;
+  
+  return 0;
 }
 
 static void
-set_gamma_info(output_t *output)
-{
-    XRRCrtcGamma *gamma;
-    double i1, v1, i2, v2;
-    int size, middle, last_best, last_red, last_green, last_blue;
-    CARD16 *best_array;
+set_gamma_info(output_t *output) {
+  XRRCrtcGamma *gamma;
+  double i1, v1, i2, v2;
+  int size, middle, last_best, last_red, last_green, last_blue;
+  CARD16 *best_array;
 
-    if (!output->crtc_info)
-  return;
+  if (!output->crtc_info) return;
 
-    size = XRRGetCrtcGammaSize(dpy, output->crtc_info->crtc.xid);
-    if (!size) {
-  warning("Failed to get size of gamma for output %s\n", output->output.string);
-  return;
-    }
+  size = XRRGetCrtcGammaSize(dpy, output->crtc_info->crtc.xid);
+  if (!size) {
+    warning("Failed to get size of gamma for output %s\n", output->output.string);
+    return;
+  }
 
-    gamma = XRRGetCrtcGamma(dpy, output->crtc_info->crtc.xid);
-    if (!gamma) {
-  warning("Failed to get gamma for output %s\n", output->output.string);
-  return;
-    }
+  gamma = XRRGetCrtcGamma(dpy, output->crtc_info->crtc.xid);
+  if (!gamma) {
+    warning("Failed to get gamma for output %s\n", output->output.string);
+    return;
+  }
 
-    /*
-     * Here is a bit tricky because gamma is a whole curve for each
-     * color.  So, typically, we need to represent 3 * 256 values as 3 + 1
-     * values.  Therefore, we approximate the gamma curve (v) by supposing
-     * it always follows the way we set it: a power function (i^g)
-     * multiplied by a brightness (b).
-     * v = i^g * b
-     * so g = (ln(v) - ln(b))/ln(i)
-     * and b can be found using two points (v1,i1) and (v2, i2):
-     * b = e^((ln(v2)*ln(i1) - ln(v1)*ln(i2))/ln(i1/i2))
-     * For the best resolution, we select i2 at the highest place not
-     * clamped and i1 at i2/2. Note that if i2 = 1 (as in most normal
-     * cases), then b = v2.
-     */
-    last_red = find_last_non_clamped(gamma->red, size);
-    last_green = find_last_non_clamped(gamma->green, size);
-    last_blue = find_last_non_clamped(gamma->blue, size);
-    best_array = gamma->red;
-    last_best = last_red;
-    if (last_green > last_best) {
-  last_best = last_green;
-  best_array = gamma->green;
-    }
-    if (last_blue > last_best) {
-  last_best = last_blue;
-  best_array = gamma->blue;
-    }
-    if (last_best == 0)
-  last_best = 1;
+  /*
+    * Here is a bit tricky because gamma is a whole curve for each
+    * color.  So, typically, we need to represent 3 * 256 values as 3 + 1
+    * values.  Therefore, we approximate the gamma curve (v) by supposing
+    * it always follows the way we set it: a power function (i^g)
+    * multiplied by a brightness (b).
+    * v = i^g * b
+    * so g = (ln(v) - ln(b))/ln(i)
+    * and b can be found using two points (v1,i1) and (v2, i2):
+    * b = e^((ln(v2)*ln(i1) - ln(v1)*ln(i2))/ln(i1/i2))
+    * For the best resolution, we select i2 at the highest place not
+    * clamped and i1 at i2/2. Note that if i2 = 1 (as in most normal
+    * cases), then b = v2.
+    */
+  last_red = find_last_non_clamped(gamma->red, size);
+  last_green = find_last_non_clamped(gamma->green, size);
+  last_blue = find_last_non_clamped(gamma->blue, size);
+  best_array = gamma->red;
+  last_best = last_red;
+  
+  if (last_green > last_best) {
+    last_best = last_green;
+    best_array = gamma->green;
+  }
+  
+  if (last_blue > last_best) {
+    last_best = last_blue;
+    best_array = gamma->blue;
+  }
+
+  if (last_best == 0)
+    last_best = 1;
 
     middle = last_best / 2;
     i1 = (double)(middle + 1) / size;
@@ -1015,185 +1009,184 @@ set_gamma_info(output_t *output)
     i2 = (double)(last_best + 1) / size;
     v2 = (double)(best_array[last_best]) / 65535;
     if (v2 < 0.0001) { /* The screen is black */
-  output->brightness = 0;
-  output->gamma.red = 1;
-  output->gamma.green = 1;
-  output->gamma.blue = 1;
-    } else {
-  if ((last_best + 1) == size)
-      output->brightness = v2;
-  else
-      output->brightness = exp((log(v2)*log(i1) - log(v1)*log(i2))/log(i1/i2));
-  output->gamma.red = log((double)(gamma->red[last_red / 2]) / output->brightness
-        / 65535) / log((double)((last_red / 2) + 1) / size);
-  output->gamma.green = log((double)(gamma->green[last_green / 2]) / output->brightness
-          / 65535) / log((double)((last_green / 2) + 1) / size);
-  output->gamma.blue = log((double)(gamma->blue[last_blue / 2]) / output->brightness
-         / 65535) / log((double)((last_blue / 2) + 1) / size);
-    }
+    output->brightness = 0;
+    output->gamma.red = 1;
+    output->gamma.green = 1;
+    output->gamma.blue = 1;
 
-    XRRFreeGamma(gamma);
+  } else {
+
+    if ((last_best + 1) == size) output->brightness = v2;
+    else output->brightness = exp((log(v2)*log(i1) - log(v1)*log(i2))/log(i1/i2));
+    
+    output->gamma.red = log((double)(gamma->red[last_red / 2]) / output->brightness
+          / 65535) / log((double)((last_red / 2) + 1) / size);
+    output->gamma.green = log((double)(gamma->green[last_green / 2]) / output->brightness
+            / 65535) / log((double)((last_green / 2) + 1) / size);
+    output->gamma.blue = log((double)(gamma->blue[last_blue / 2]) / output->brightness
+          / 65535) / log((double)((last_blue / 2) + 1) / size);
+  } // else
+
+  XRRFreeGamma(gamma);
 }
 
 static void
-set_output_info (output_t *output, RROutput xid, XRROutputInfo *output_info)
-{
-    /* sanity check output info */
-    if (output_info->connection != RR_Disconnected && !output_info->nmode)
-  warning ("Output %s is not disconnected but has no modes\n",
-     output_info->name);
+set_output_info (output_t *output, RROutput xid, XRROutputInfo *output_info) {
+  /* sanity check output info */
+  if (output_info->connection != RR_Disconnected && !output_info->nmode)
+    warning ("Output %s is not disconnected but has no modes\n",
+      output_info->name);
     
-    /* set output name and info */
-    if (!(output->output.kind & name_xid))
-  set_name_xid (&output->output, xid);
-    if (!(output->output.kind & name_string))
-  set_name_string (&output->output, output_info->name);
-    output->output_info = output_info;
+  /* set output name and info */
+  if (!(output->output.kind & name_xid))
+    set_name_xid (&output->output, xid);
+  if (!(output->output.kind & name_string))
+    set_name_string (&output->output, output_info->name);
+  output->output_info = output_info;
     
-    /* set crtc name and info */
-    if (!(output->changes & changes_crtc))
-  set_name_xid (&output->crtc, output_info->crtc);
+  /* set crtc name and info */
+  if (!(output->changes & changes_crtc))
+    set_name_xid (&output->crtc, output_info->crtc);
     
-    if (output->crtc.kind == name_xid && output->crtc.xid == None)
-  output->crtc_info = NULL;
-    else
-    {
-  output->crtc_info = find_crtc (&output->crtc);
-  if (!output->crtc_info)
-  {
+  if (output->crtc.kind == name_xid && output->crtc.xid == None)
+    output->crtc_info = NULL;
+  else {
+  
+    output->crtc_info = find_crtc (&output->crtc);
+    if (!output->crtc_info) {
       if (output->crtc.kind & name_xid)
-    fatal ("cannot find crtc 0x%x\n", output->crtc.xid);
+        fatal ("cannot find crtc 0x%x\n", output->crtc.xid);
       if (output->crtc.kind & name_index)
-    fatal ("cannot find crtc %d\n", output->crtc.index);
-  }
-  if (!output_can_use_crtc (output, output->crtc_info))
+        fatal ("cannot find crtc %d\n", output->crtc.index);
+    } // !output->crtc_info
+
+    if (!output_can_use_crtc (output, output->crtc_info))
       fatal ("output %s cannot use crtc 0x%x\n", output->output.string,
        output->crtc_info->crtc.xid);
-    }
+  } // else
 
-    /* set mode name and info */
-    if (!(output->changes & changes_mode))
-    {
-  crtc_t	*crtc = NULL;
-  
-  if (output_info->crtc)
+  /* set mode name and info */
+  if (!(output->changes & changes_mode)) {
+    crtc_t	*crtc = NULL;
+    
+    if (output_info->crtc) 
       crtc = find_crtc_by_xid(output_info->crtc);
-  if (crtc && crtc->crtc_info)
+    
+    if (crtc && crtc->crtc_info)
       set_name_xid (&output->mode, crtc->crtc_info->mode);
-  else if (output->crtc_info)
+    
+    else if (output->crtc_info)
       set_name_xid (&output->mode, output->crtc_info->crtc_info->mode);
-  else
-      set_name_xid (&output->mode, None);
-  if (output->mode.xid)
-  {
+      else set_name_xid (&output->mode, None);
+    
+    if (output->mode.xid) {
       output->mode_info = find_mode_by_xid (output->mode.xid);
+      
       if (!output->mode_info)
-    fatal ("server did not report mode 0x%x for output %s\n",
-           output->mode.xid, output->output.string);
-  }
-  else
-      output->mode_info = NULL;
-    }
-    else if (output->mode.kind == name_xid && output->mode.xid == None)
-  output->mode_info = NULL;
-    else
-    {
-  if (output->mode.kind == name_preferred)
-      output->mode_info = preferred_mode (output);
-  else
-      output->mode_info = find_mode_for_output (output, &output->mode);
-  if (!output->mode_info)
-  {
+        fatal ("server did not report mode 0x%x for output %s\n",
+          output->mode.xid, output->output.string);
+
+    } else output->mode_info = NULL;
+  } // !(output->changes & changes_mode)
+  
+  else if (output->mode.kind == name_xid && output->mode.xid == None)
+    output->mode_info = NULL;
+
+  else {
+
+    if (output->mode.kind == name_preferred)
+        output->mode_info = preferred_mode (output);
+    else output->mode_info = find_mode_for_output (output, &output->mode);
+  
+    if (!output->mode_info) {
       if (output->mode.kind & name_preferred)
-    fatal ("cannot find preferred mode\n");
+        fatal ("cannot find preferred mode\n");
       if (output->mode.kind & name_string)
-    fatal ("cannot find mode %s\n", output->mode.string);
+        fatal ("cannot find mode %s\n", output->mode.string);
       if (output->mode.kind & name_xid)
-    fatal ("cannot find mode 0x%x\n", output->mode.xid);
-  }
-  if (!output_can_use_mode (output, output->mode_info))
-      fatal ("output %s cannot use mode %s\n", output->output.string,
-       output->mode_info->name);
+        fatal ("cannot find mode 0x%x\n", output->mode.xid);
     }
 
-    /* set position */
-    if (!(output->changes & changes_position))
-    {
-  if (output->crtc_info)
-  {
+    if (!output_can_use_mode (output, output->mode_info))
+      fatal ("output %s cannot use mode %s\n", output->output.string,
+        output->mode_info->name);
+  
+  } // isolated else
+
+  /* set position */
+  if (!(output->changes & changes_position)) {
+    if (output->crtc_info) {
       output->x = output->crtc_info->crtc_info->x;
       output->y = output->crtc_info->crtc_info->y;
-  }
-  else
-  {
+    } else {
       output->x = 0;
       output->y = 0;
-  }
     }
+  } // !(output->changes & changes_position)
 
-    /* set rotation */
-    if (!(output->changes & changes_rotation))
-    {
-  output->rotation &= ~0xf;
-  if (output->crtc_info)
+  /* set rotation */
+  if (!(output->changes & changes_rotation)) {
+    output->rotation &= ~0xf;
+    if (output->crtc_info)
       output->rotation |= (output->crtc_info->crtc_info->rotation & 0xf);
-  else
+    else
       output->rotation = RR_Rotate_0;
-    }
-    if (!(output->changes & changes_reflection))
-    {
-  output->rotation &= ~(RR_Reflect_X|RR_Reflect_Y);
-  if (output->crtc_info)
+  } // changes_rotation
+  
+  if (!(output->changes & changes_reflection)) {
+    output->rotation &= ~(RR_Reflect_X|RR_Reflect_Y);
+    if (output->crtc_info)
       output->rotation |= (output->crtc_info->crtc_info->rotation &
-         (RR_Reflect_X|RR_Reflect_Y));
-    }
-    if (!output_can_use_rotation (output, output->rotation))
-  fatal ("output %s cannot use rotation \"%s\" reflection \"%s\"\n",
-         output->output.string,
-         rotation_name (output->rotation),
-         reflection_name (output->rotation));
+        (RR_Reflect_X|RR_Reflect_Y));
+  } // changes_reflection
 
-    /* set gamma */
-    if (!(output->changes & changes_gamma))
-      set_gamma_info(output);
+  if (!output_can_use_rotation (output, output->rotation))
+    fatal (
+      "output %s cannot use rotation \"%s\" reflection \"%s\"\n",
+      output->output.string,
+      rotation_name (output->rotation),
+      reflection_name (output->rotation));
 
-    /* set transformation */
-    if (!(output->changes & changes_transform))
-    {
-  if (output->crtc_info)
+  /* set gamma */
+  if (!(output->changes & changes_gamma)) set_gamma_info(output);
+
+  /* set transformation */
+  if (!(output->changes & changes_transform)) {
+    if (output->crtc_info)
       copy_transform (&output->transform, &output->crtc_info->current_transform);
-  else
-      init_transform (&output->transform);
-    } else {
-  /* transform was already set for --scale or --transform */
+    else init_transform (&output->transform);
+    
+  } else {
+    /* transform was already set for --scale or --transform */
 
-  /* for --scale-from, figure out the mode size and compute the transform
-   * for the target framebuffer area */
-  if (output->scale_from_w > 0 && output->mode_info) {
+    /* for --scale-from, figure out the mode size and compute the transform
+      * for the target framebuffer area */
+    if (output->scale_from_w > 0 && output->mode_info) {
+      
       double sx = (double)output->scale_from_w /
         output->mode_info->width;
       double sy = (double)output->scale_from_h /
         output->mode_info->height;
-      if (verbose)
-    printf("scaling %s by %lfx%lf\n", output->output.string, sx,
-           sy);
+
+      if (verbose) printf(
+        "scaling %s by %lfx%lf\n", output->output.string, sx, sy);
+
       init_transform (&output->transform);
       output->transform.transform.matrix[0][0] = XDoubleToFixed (sx);
       output->transform.transform.matrix[1][1] = XDoubleToFixed (sy);
       output->transform.transform.matrix[2][2] = XDoubleToFixed (1.0);
-      if (sx != 1 || sy != 1)
-    output->transform.filter = "bilinear";
-      else
-    output->transform.filter = "nearest";
+      
+      if (sx != 1 || sy != 1) output->transform.filter = "bilinear";
+      else output->transform.filter = "nearest";
+      
       output->transform.nparams = 0;
       output->transform.params = NULL;
-  }
-    }
+    } // output->scale_from_w > 0 && output->mode_info
+  } // [else] !(output->changes & changes_transform
 
-    /* set primary */
-    if (!(output->changes & changes_primary))
-  output->primary = output_is_primary(output);
-}
+  /* set primary */
+  if (!(output->changes & changes_primary)) output->primary = output_is_primary(output);
+} // set_output_info
     
 static void
 get_screen (Bool current)
